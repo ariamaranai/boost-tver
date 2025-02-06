fe30d5b57fd3beab7e9b739f2b77f19c7be717c3
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-07 02:22:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,25 @@
   o.freeze = a => a;
   o.isFrozen = () => 0;
 
+  o.defineProperty(self, "trustedTypes", {
+    get: () => {}
+  });
+  
   let setter = { set: () => 0 };
   o.defineProperties(HTMLImageElement.prototype, {
     onerror: setter,
     onload: setter,
     src: setter
   });
+  o.defineProperties(HTMLScriptElement.prototype, {
+    onerror: setter,
+    src: {
+      set (a) {
+        a != "/doubleserve.js?adname=x" &&
+        a != "https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js" &&
+        a.slice(12, 18) != "google" &&
+          Element.prototype.setAttribute.call(this, "src", a)
+      }
+    }
+  })
 }
