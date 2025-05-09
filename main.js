4859478976f62175a7cfa48e77b0c9adf6ddda1f
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-09 13:57:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,29 @@
   let o = Object;
   o.freeze = a => a;
   o.isFrozen = () => 0;
-  o.defineProperty(self, "trustedTypes", {
-    get: () => {}
+  o.defineProperties(self, {
+    dataLayer: { value: 0 },
+    trustedTypes: { get: () => {} }
   });
   o.prototype.hasOwnProperty.call = (a, b) => b in a;
+  o.defineProperties(HTMLScriptElement.prototype, {
+    src: {
+      set (a) {
+        a != "/doubleserve.js?adname=x" &&
+        a != "https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js" &&
+        a.slice(12, 18) != "google" &&
+          Element.prototype.setAttribute.call(this, "src", a)
+      }
+    },
+    onerror: { set: () => 0 },
+    timeout: { set: () => 0 }
+  });
+
   let none = o.seal(0);
-  Image = function () {
-    return none;
-  }
-  o.defineProperty(HTMLScriptElement.prototype, "src", {
-    set (a) {
-      a != "/doubleserve.js?adname=x" &&
-      a != "https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js" &&
-      a.slice(12, 18) != "google" &&
-        Element.prototype.setAttribute.call(this, "src", a)
-    }
+  Image = function () { return none }
+
+  let d = document;
+  d.addEventListener("DOMContentLoaded", () => {
+    d.head.appendChild(d.createElement('script')).src = "//cdn.ic.tver.jp/js/integralCoreCommon.js"
   });
 }
